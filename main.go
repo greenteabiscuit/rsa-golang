ff92977bd2f77734d547fe5beea8465060bb1d30
reishi mitani <mitani-reishi116@g.ecc.u-tokyo.ac.jp>
2021-06-21 00:50:52 +0900

    first commit

---
+++ main.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"fmt"
+)
+
+type publicKey struct {
+	E int
+	N int
+}
+
+type privateKey struct {
+	D int
+	N int
+}
+
+func main() {
+	publicKey, privateKey := generateKeys(101, 3259)
+	plainText := "This is a secret"
+	encryptedText := encrypt(plainText, publicKey)
+	decrypt(encryptedText, privateKey)
+}
+
+func generateKeys(p int, q int) (public publicKey, secret privateKey) {
+	N := p * q
+	L := lcm(p-1, q-1)
+	var D, E int
+	for i := 2; i < L; i++ {
+		if gcd(i, L) == 1 {
+			E = i
+			break
+		}
+	}
+	for i := 2; i < L; i++ {
+		if (E*i)%L == 1 {
+			D = i
+			break
+		}
+	}
+	return publicKey{E: E, N: N}, privateKey{D: D, N: N}
+}
+
+func lcm(p int, q int) int {
+	return (p * q) / gcd(p, q)
+}
+
+func gcd(p int, q int) int {
+	// https://play.golang.org/p/SmzvkDjYlb
+	for q != 0 {
+		t := q
+		q = p % q
+		p = t
+	}
+	return p
+}
+
+func encrypt(plainText string, publicKey publicKey) string {
+	E, N := publicKey.E, publicKey.N
+	fmt.Println(E, N)
+	resultString := ""
+	// fmt.Println(int('あ'))
+	fmt.Println(string(rune('a')))
+	for _, char := range plainText {
+		res := 1
+		for i := 0; i < E; i++ {
+			res *= int(char)
+			res %= N
+		}
+
+		resultString += string(rune(res))
+	}
+	fmt.Println(resultString)
+	return resultString
+}
+
+func decrypt(encryptedText string, privateKey privateKey) {
+	D, N := privateKey.D, privateKey.N
+	resultString := ""
+	for _, char := range encryptedText {
+		res := 1
+		for i := 0; i < D; i++ {
+			res *= int(char)
+			res %= N
+		}
+
+		resultString += string(rune(res))
+	}
+	fmt.Println(resultString)
+}
